deca60917e39ced1200efb9b701ee023cfdb8416
wheynelau <waynelau15045@gmail.com>
2024-05-20 01:06:51 +0800

    refactor iterations

--- calculate-pi/src/main.rs
+++ calculate-pi/src/main.rs
@@ -3,21 +3,13 @@ use std::env;
 
 fn main() {
 
-    let mut rng = rand::thread_rng();
+    let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
     let mut inside_circle = 0;
 
-    /* Honestly this was copied, still trying to understand it. From how i see it,
-    it tries to get the value from the ITER environment variable, then tries to parse it as an integer
-
-    Maybe similar to try except in python
-
-        os.environ['ITER'], although would be nice if there's something like getenv
-     */
-    // Attempt to get the environment variable
-    let iter_var_result = env::var("ITER");
+    let iter_var_result: Result<String, env::VarError> = env::var("ITER");
 
     // Handle the case where the environment variable is not found or cannot be parsed as i32
-    let iterations = match iter_var_result {
+    let iterations: i32 = match iter_var_result {
         Ok(iter_str) => match iter_str.parse::<i32>() {
             Ok(iterations) => {
                 println!("ITER is: {}", iterations);
@@ -37,18 +29,18 @@ fn main() {
     // Use iterations here
     println!("Final value of iterations: {}", iterations);
 
-    let start = std::time::Instant::now();
+    let start: std::time::Instant = std::time::Instant::now();
     for _ in 0..iterations {
-        let x = rng.gen_range(-1.0..1.0);
-        let y = rng.gen_range(-1.0..1.0);
+        let x: f64 = rng.gen_range(-1.0..1.0);
+        let y: f64 = rng.gen_range(-1.0..1.0);
         if x * x + y * y <= 1.0 {
             inside_circle += 1;
         }
     }
 
-    let duration = start.elapsed();
+    let duration: std::time::Duration = start.elapsed();
 
     println!("Time elapsed is: {:?}", duration);
-    let pi = 4.0 * (inside_circle as f64) / (iterations as f64);
+    let pi: f64 = 4.0 * (inside_circle as f64) / (iterations as f64);
     println!("π is roughly {}", pi);
 }
