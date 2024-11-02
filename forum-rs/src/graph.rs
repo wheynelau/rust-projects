8e5428b312356f0d6bc875d4b7c819ba7c654d57
wheynelau <waynelau15045@gmail.com>
2024-11-02 15:35:15 +0800

    remove debug statement for node count in ThreadGraph

--- forum-rs/src/graph.rs
+++ forum-rs/src/graph.rs
@@ -90,7 +90,7 @@ impl ThreadGraph {
         // self.show_roots();
         // let mut root_id: String = String::new();
         // print number of nodes
-        dbg!(self.graph.node_count());
+        // dbg!(self.graph.node_count());
         let mut final_threads: Vec<(String, Vec<String>)> = Vec::with_capacity(10000);
         for start in self.threads.iter() {
             // skip if not root
